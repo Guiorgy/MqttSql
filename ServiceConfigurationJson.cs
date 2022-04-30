c33179d118d2d5d09d03e43f96010ad6a3f15e5f
Guiorgy <Guiorgy123@gmail.com>
2022-04-30 21:28:22 +0400

    allow more keys and types in json config
    - Allow Subscriptions and Databases inside them to be defined as either a single object, or array of objects
    - Allow the definitions of Subscriptions and Databases inside them with multiple keys (singular and plural)

--- ServiceConfigurationJson.cs
+++ ServiceConfigurationJson.cs
@@ -2,21 +2,24 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
-using static MqttSql.Configurations.BaseConfiguration;
+using static MqttSql.Configurations.DatabaseConfiguration;
 
 namespace MqttSql.ConfigurationsJson
 {
     public sealed class ServiceConfiguration
     {
-        public BaseConfiguration[] Databases { get; }
+        [JsonConverter(typeof(SingleOrArrayJsonConverter))]
+        public DatabaseConfiguration[] Databases { get; }
+        [JsonConverter(typeof(SingleOrArrayJsonConverter))]
         public BrokerConfiguration[] Brokers { get; }
 
         [JsonConstructor]
         public ServiceConfiguration(
-            BaseConfiguration[]? databases = default,
+            DatabaseConfiguration[]? databases = default,
             BrokerConfiguration[]? brokers = default) =>
-            (Databases, Brokers) = (databases ?? Array.Empty<BaseConfiguration>(), brokers ?? Array.Empty<BrokerConfiguration>());
+            (Databases, Brokers) = (databases ?? Array.Empty<DatabaseConfiguration>(), brokers ?? Array.Empty<BrokerConfiguration>());
 
         public override string ToString()
         {
@@ -31,14 +34,14 @@ namespace MqttSql.ConfigurationsJson
         }
     }
 
-    public sealed class BaseConfiguration : IEquatable<BaseConfiguration>
+    public sealed class DatabaseConfiguration : IEquatable<DatabaseConfiguration>
     {
         public string Name { get; }
         public string Type { get; }
         public string ConnectionString { get; }
 
         [JsonConstructor]
-        public BaseConfiguration(
+        public DatabaseConfiguration(
             string name = "sqlite",
             string type = nameof(DatabaseType.SQLite),
             string connectionString = "") =>
@@ -54,13 +57,13 @@ namespace MqttSql.ConfigurationsJson
 
         internal StringBuilder ToStringBuilder()
         {
-            return new StringBuilder(10)
+            return new StringBuilder(6)
                 .Append("\tName: ").AppendLine(Name)
-                .Append("\tType: ").AppendLine(Type.ToString())
+                .Append("\tType: ").AppendLine(Type)
                 .Append("\tConnectionString: ").AppendLine(ConnectionString);
         }
 
-        public bool Equals([AllowNull] BaseConfiguration other)
+        public bool Equals([AllowNull] DatabaseConfiguration other)
         {
             return
                 other != null
@@ -70,7 +73,7 @@ namespace MqttSql.ConfigurationsJson
 
         public override bool Equals(object? obj)
         {
-            return Equals(obj as BaseConfiguration);
+            return Equals(obj as DatabaseConfiguration);
         }
 
         public override int GetHashCode()
@@ -81,10 +84,17 @@ namespace MqttSql.ConfigurationsJson
 
     public sealed class BrokerConfiguration : IEquatable<BrokerConfiguration>
     {
+        private const string DeprecationMessage = "This is defined as a workaround to map 2 different names onto the same property when deserializing, while only serializing one. Use the `Subscriptions` property instead.";
+
         public string Host { get; }
         public int Port { get; }
         public string User { get; }
         public string Password { get; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [Obsolete(DeprecationMessage, true)]
+        [JsonConverter(typeof(SingleOrArrayJsonConverter<SubscriptionConfiguration, SubscriptionConfiguration.SubscriptionConfigurationJsonConverter>))]
+        public SubscriptionConfiguration[]? Subscription { get => default; }
+        [JsonConverter(typeof(SingleOrArrayJsonConverter<SubscriptionConfiguration, SubscriptionConfiguration.SubscriptionConfigurationJsonConverter>))]
         public SubscriptionConfiguration[] Subscriptions { get; }
 
         [JsonConstructor]
@@ -93,8 +103,9 @@ namespace MqttSql.ConfigurationsJson
             int port = 1883,
             string user = "",
             string password = "",
+            SubscriptionConfiguration[]? subscription = default,
             SubscriptionConfiguration[]? subscriptions = default) =>
-            (Host, Port, User, Password, Subscriptions) = (host, port, user, password, subscriptions ?? Array.Empty<SubscriptionConfiguration>());
+            (Host, Port, User, Password, Subscriptions) = (host, port, user, password, subscriptions ?? subscription ?? Array.Empty<SubscriptionConfiguration>());
 
         public override string ToString()
         {
@@ -153,40 +164,208 @@ namespace MqttSql.ConfigurationsJson
 
     public sealed class SubscriptionConfiguration
     {
+        private const string DeprecationMessage = "This is defined as a workaround to map 2 different names onto the same property when deserializing, while only serializing one. Use the `Databases` property instead.";
+
+        private const string TopicDefault = "sql";
+        private const int QOSDefault = 2;
+        private const TableConfiguration[]? DbDefault = default;
+        private const TableConfiguration[]? DatabaseDefault = default;
+        private const TableConfiguration[]? DbsDefault = default;
+        private const TableConfiguration[] DatabasesDefault = default;
+
         public string Topic { get; }
         public int QOS { get; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [Obsolete(DeprecationMessage, true)]
+        [JsonConverter(typeof(SingleOrArrayJsonConverter))]
         [JsonPropertyName("base")]
-        public string Database { get; }
-        public string Table { get; }
-        public string TimestampFormat { get; }
+        public TableConfiguration[]? Db { get => default; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [Obsolete(DeprecationMessage, true)]
+        [JsonConverter(typeof(SingleOrArrayJsonConverter))]
+        public TableConfiguration[]? Database { get => default; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [Obsolete(DeprecationMessage, true)]
+        [JsonConverter(typeof(SingleOrArrayJsonConverter))]
+        [JsonPropertyName("bases")]
+        public TableConfiguration[]? Dbs { get => default; }
+        [JsonConverter(typeof(SingleOrArrayJsonConverter))]
+        public TableConfiguration[] Databases { get; }
 
         [JsonConstructor]
         public SubscriptionConfiguration(
-            string topic = "sql",
-            int qos = 2,
-            string database = "sqlite",
-            string table = "mqtt",
-            string timestampFormat = "yyyy-MM-dd-HH:mm:ss") =>
-            (Topic, QOS, Database, Table, TimestampFormat) = (topic, qos, database, table, timestampFormat);
+            string topic = TopicDefault,
+            int qos = QOSDefault,
+            TableConfiguration[]? db = DbDefault,
+            TableConfiguration[]? database = DatabaseDefault,
+            TableConfiguration[]? dbs = DbsDefault,
+            TableConfiguration[]? databases = DatabasesDefault) =>
+            (Topic, QOS, Databases) = (topic, qos, databases ?? dbs ?? database ?? db ?? Array.Empty<TableConfiguration>());
 
         public override string ToString()
         {
             return
                 $"Topic: {Topic}{Environment.NewLine}" +
                 $"QOS: {QOS}{Environment.NewLine}" +
-                $"Database: {Database}{Environment.NewLine}" +
+                (
+                    Databases.Length == 1 ? (
+                        $"Database: {Databases[0].DatabaseName}{Environment.NewLine}" +
+                        $"Table: {Databases[0].Table}{Environment.NewLine}" +
+                        $"TimestampFormat: {Databases[0].TimestampFormat}{Environment.NewLine}"
+                    ) : (
+                        $"Databases:{Environment.NewLine}" +
+                            string.Join(Environment.NewLine,
+                                Databases.Select(db => db.ToString().AppendBeforeLines("\t")))
+                    )
+                );
+        }
+
+        internal StringBuilder ToStringBuilder()
+        {
+            var builder = new StringBuilder(10)
+                .Append("\t\tTopic: ").AppendLine(Topic)
+                .Append("\t\tQOS: ").AppendLine(QOS.ToString());
+            if (Databases.Length == 1)
+            {
+                builder.Append("\t\tDatabase: ").AppendLine(Databases[0].DatabaseName)
+                    .Append("\t\tTable: ").AppendLine(Databases[0].Table)
+                    .Append("\t\tTimestampFormat: ").AppendLine(Databases[0].TimestampFormat);
+            }
+            else
+            {
+                builder.AppendLine("\t\tDatabases: ");
+                if (Databases.Length == 0)
+                    builder.AppendLine();
+                else foreach (var db in Databases)
+                        builder.Append(db.ToStringBuilder()).AppendLine();
+            }
+            return builder;
+        }
+
+        public sealed class SubscriptionConfigurationJsonConverter : JsonConverter<SubscriptionConfiguration>
+        {
+            public override SubscriptionConfiguration? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+            {
+                if (reader.TokenType != JsonTokenType.StartObject) throw new JsonException();
+
+                bool isFlat = false;
+                Utf8JsonReader probeReader = reader;
+                while (probeReader.Read())
+                {
+                    if (probeReader.TokenType == JsonTokenType.EndObject) break;
+
+                    if (probeReader.TokenType == JsonTokenType.PropertyName)
+                    {
+                        string? propertyName = probeReader.GetString();
+
+                        if (propertyName?.ToLower() == "bases" || propertyName?.ToLower() == "databases")
+                        {
+                            isFlat = false;
+                            break;
+                        }
+
+                        if (propertyName?.ToLower() == "base" || propertyName?.ToLower() == "database")
+                        {
+                            probeReader.Read();
+
+                            if (probeReader.TokenType == JsonTokenType.StartArray)
+                            {
+                                isFlat = false;
+                                break;
+                            }
+
+                            if (probeReader.TokenType == JsonTokenType.String) isFlat = true;
+                        }
+
+                        if (propertyName?.ToLower() == "table" || propertyName?.ToLower() == "timestampformat") isFlat = true;
+                    }
+                }
+
+                if (isFlat)
+                {
+                    var flat = JsonSerializer.Deserialize<FlatConfiguration>(ref reader, options);
+                    if (flat != null && (flat.Database != null || flat.Table != null))
+                    {
+                        return new SubscriptionConfiguration(
+                            topic: flat.Topic ?? TopicDefault,
+                            qos: flat.QOS ?? QOSDefault,
+                            databases: new TableConfiguration[]
+                            {
+                                new TableConfiguration(
+                                    databaseName: flat.Database ?? TableConfiguration.DatabaseNameDefault,
+                                    table: flat.Table ?? TableConfiguration.TableDefault,
+                                    timestampFormat: flat.TimestampFormat ?? TableConfiguration.TimestampFormatDefault
+                                )
+                            }
+                        );
+                    }
+                    else return null;
+                }
+                else return JsonSerializer.Deserialize<SubscriptionConfiguration>(ref reader, options);
+            }
+
+            public override void Write(Utf8JsonWriter writer, SubscriptionConfiguration values, JsonSerializerOptions options)
+            {
+                JsonSerializer.Serialize(writer, values, options);
+            }
+
+            public sealed class FlatConfiguration
+            {
+                public string? Topic { get; }
+                public int? QOS { get; }
+                [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+                [Obsolete(DeprecationMessage, true)]
+                [JsonPropertyName("base")]
+                public string? Db { get => default; }
+                public string? Database { get; }
+                public string? Table { get; }
+                public string? TimestampFormat { get; }
+
+                [JsonConstructor]
+                public FlatConfiguration(
+                    string? topic = null,
+                    int? qos = null,
+                    string? db = null,
+                    string? database = null,
+                    string? table = null,
+                    string? timestampFormat = null) =>
+                    (Topic, QOS, Database, Table, TimestampFormat) = (topic, qos, database ?? db, table, timestampFormat);
+            }
+        }
+    }
+
+    public sealed class TableConfiguration
+    {
+        internal const string DatabaseNameDefault = "sqlite";
+        internal const string TableDefault = "mqtt";
+        internal const string TimestampFormatDefault = "yyyy-MM-dd-HH:mm:ss";
+
+        [JsonPropertyName("name")]
+        public string DatabaseName { get; }
+        public string Table { get; }
+        public string TimestampFormat { get; }
+
+        [JsonConstructor]
+        public TableConfiguration(
+            string databaseName = DatabaseNameDefault,
+            string table = TableDefault,
+            string timestampFormat = TimestampFormatDefault) =>
+            (DatabaseName, Table, TimestampFormat) = (databaseName, table, timestampFormat);
+
+        public override string ToString()
+        {
+            return
+                $"DatabaseName: {DatabaseName}{Environment.NewLine}" +
                 $"Table: {Table}{Environment.NewLine}" +
                 $"TimestampFormat: {TimestampFormat}{Environment.NewLine}";
         }
 
         internal StringBuilder ToStringBuilder()
         {
-            return new StringBuilder(10)
-                .Append("\t\tTopic: ").AppendLine(Topic)
-                .Append("\t\tQOS: ").AppendLine(QOS.ToString())
-                .Append("\t\tDatabase: ").AppendLine(Database)
-                .Append("\t\tTable: ").AppendLine(Table)
-                .Append("\t\tTimestampFormat: ").AppendLine(TimestampFormat);
+            return new StringBuilder(6)
+                .Append("\t\t\tDatabaseName: ").AppendLine(DatabaseName)
+                .Append("\t\t\tTable: ").AppendLine(Table)
+                .Append("\t\t\tTimestampFormat: ").AppendLine(TimestampFormat);
         }
     }
 }
---
+++ SingleOrArrayJsonConverter.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace MqttSql
+{
+    public sealed class SingleOrArrayJsonConverter : JsonConverterFactory
+	{
+		private JsonConverterFactory? _jsonConverterFactory = null;
+
+		private JsonConverterFactory GetJsonConverterFactory(Type typeToConvert)
+		{
+			if (_jsonConverterFactory == null)
+			{
+				ArgumentNullException.ThrowIfNull(typeToConvert, nameof(typeToConvert));
+				if (!typeToConvert.IsArray) throw new ArgumentException($"{nameof(typeToConvert)} must be an Array!");
+				Type valueType = typeToConvert.GetElementType()!;
+				_jsonConverterFactory =
+					(JsonConverterFactory)Activator.CreateInstance(
+						typeof(SingleOrArrayJsonConverter<>).MakeGenericType(valueType),
+						BindingFlags.Instance | BindingFlags.Public,
+						binder: null,
+						args: null,
+						culture: null)!;
+			}
+			return _jsonConverterFactory;
+		}
+
+		public override bool CanConvert(Type typeToConvert)
+		{
+			if (typeToConvert.IsArray)
+				return GetJsonConverterFactory(typeToConvert).CanConvert(typeToConvert);
+			else
+				return false;
+		}
+
+		public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+		{
+			return GetJsonConverterFactory(typeToConvert).CreateConverter(typeToConvert, options)!;
+		}
+	}
+
+	public sealed class SingleOrArrayJsonConverter<TValue> : JsonConverterFactory
+	{
+		public override bool CanConvert(Type typeToConvert)
+		{
+			return typeToConvert == typeof(TValue[]);
+		}
+
+		public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+		{
+			return (JsonConverter)Activator.CreateInstance(
+				typeof(SingleOrArrayJsonConverterInner),
+				BindingFlags.Instance | BindingFlags.Public,
+				binder: null,
+				args: new object[] { options },
+				culture: null)!;
+		}
+
+		private sealed class SingleOrArrayJsonConverterInner : JsonConverter<TValue[]>
+		{
+			private readonly Type _singleType;
+			private readonly JsonConverter<TValue> _singleConverter;
+			private readonly Type _arrayType;
+			private readonly JsonConverter<TValue[]> _arrayConverter;
+
+#pragma warning disable S1144 // Unused private types or members should be removed
+            public SingleOrArrayJsonConverterInner(JsonSerializerOptions options)
+			{
+				_singleType = typeof(TValue);
+				_singleConverter = (JsonConverter<TValue>)options.GetConverter(_singleType);
+				_arrayType = typeof(TValue[]);
+				_arrayConverter = (JsonConverter<TValue[]>)options.GetConverter(_arrayType);
+			}
+#pragma warning restore S1144 // Unused private types or members should be removed
+
+            public override TValue[]? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+			{
+                if (reader.TokenType == JsonTokenType.StartArray)
+				{
+                    return _arrayConverter.Read(ref reader, _arrayType, options);
+				}
+				else
+				{
+					var value = _singleConverter.Read(ref reader, _singleType, options);
+					if (value != null) return new TValue[1] { value };
+					return default;
+				}
+			}
+
+			public override void Write(Utf8JsonWriter writer, TValue[] values, JsonSerializerOptions options)
+			{
+				_arrayConverter.Write(writer, values, options);
+			}
+		}
+	}
+
+	public sealed class SingleOrArrayJsonConverter<TValue, TJsonConverter> : JsonConverterFactory where TJsonConverter : JsonConverter<TValue>
+	{
+		public override bool CanConvert(Type typeToConvert)
+		{
+			return typeToConvert == typeof(TValue[]);
+		}
+
+		public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+		{
+			return (JsonConverter)Activator.CreateInstance(
+				typeof(SingleOrArrayJsonConverterInner),
+				BindingFlags.Instance | BindingFlags.Public,
+				binder: null,
+				args: null,
+				culture: null)!;
+		}
+
+		private sealed class SingleOrArrayJsonConverterInner : JsonConverter<TValue[]>
+		{
+			private readonly Type _singleType;
+			private readonly JsonConverter<TValue> _singleConverter;
+			private readonly Type _arrayType;
+			private readonly JsonConverter<TValue[]> _arrayConverter;
+
+#pragma warning disable S1144 // Unused private types or members should be removed
+			public SingleOrArrayJsonConverterInner()
+			{
+				_singleType = typeof(TValue);
+				_singleConverter =
+					(JsonConverter<TValue>)Activator.CreateInstance(
+						typeof(TJsonConverter),
+						BindingFlags.Instance | BindingFlags.Public,
+						binder: null,
+						args: null,
+						culture: null)!;
+				_arrayType = typeof(TValue[]);
+				_arrayConverter = new ArrayJsonConverter(_singleConverter);
+			}
+#pragma warning restore S1144 // Unused private types or members should be removed
+
+			public override TValue[]? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+			{
+				if (reader.TokenType == JsonTokenType.StartArray)
+				{
+					return _arrayConverter.Read(ref reader, _arrayType, options);
+				}
+				else
+				{
+					var value = _singleConverter.Read(ref reader, _singleType, options);
+					if (value != null) return new TValue[1] { value };
+					return default;
+				}
+			}
+
+			public override void Write(Utf8JsonWriter writer, TValue[] values, JsonSerializerOptions options)
+			{
+				_arrayConverter.Write(writer, values, options);
+			}
+
+			public sealed class ArrayJsonConverter : JsonConverter<TValue[]>
+			{
+				private readonly Type _valueType;
+				private readonly JsonConverter<TValue> _valueConverter;
+
+				public ArrayJsonConverter(JsonConverter<TValue> valueConverter)
+				{
+					_valueType = typeof(TValue);
+					_valueConverter = valueConverter;
+				}
+
+				public override TValue[]? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+				{
+					if (reader.TokenType != JsonTokenType.StartArray) throw new JsonException();
+
+					List<TValue> values = new();
+					while (reader.Read())
+					{
+						if (reader.TokenType == JsonTokenType.EndArray) break;
+
+						var value = _valueConverter.Read(ref reader, _valueType, options);
+						if (value != null) values.Add(value);
+					}
+					return values.ToArray();
+				}
+
+				public override void Write(Utf8JsonWriter writer, TValue[] values, JsonSerializerOptions options)
+				{
+					writer.WriteStartArray();
+					foreach (TValue value in values)
+						_valueConverter.Write(writer, value, options);
+					writer.WriteEndArray();
+				}
+			}
+		}
+	}
+}
