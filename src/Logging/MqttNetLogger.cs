d01d08f106bf6f4e6b7f870bc257ed2481ce2d55
Guiorgy <Guiorgy123@gmail.com>
2024-02-12 16:57:37 +0400

    fixed null exception in MqttNetLogger
    parameters array in IMqttNetLogger.Publish can be null

--- src/Logging/MqttNetLogger.cs
+++ src/Logging/MqttNetLogger.cs
@@ -12,11 +12,11 @@ public sealed class MqttNetLogger(string clientId, Logger logger, Logger.LogLeve
 
     public bool IsEnabled => logger.EnabledFor(logLevel);
 
-    public void Publish(MqttNetLogLevel logLevel, string source, string message, object[] parameters, Exception exception)
+    public void Publish(MqttNetLogLevel logLevel, string source, string message, object[]? parameters, Exception exception)
     {
         if (logLevel < minMqttLogLevel) return;
 
-        message = $"[{clientId}] [{Enum.GetName(logLevel)} - {source}]\n{message}\n{string.Join('\n', parameters)}";
+        message = $"[{clientId}] [{Enum.GetName(logLevel)} - {source}]\n{message}\n{(parameters != null ? string.Join('\n', parameters) : "")}";
 
         if (exception == null)
             logger.Log(this.logLevel, message);
