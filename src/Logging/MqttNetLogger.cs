6f5bd19734898a8dc38b5cd52d5afa7e454db4ed
Guiorgy <Guiorgy123@gmail.com>
2024-03-19 21:33:27 +0400

    migrated to the GPLv3 license

--- src/Logging/MqttNetLogger.cs
+++ src/Logging/MqttNetLogger.cs
@@ -1,4 +1,11 @@
-﻿using MQTTnet.Diagnostics.Logger;
+﻿/*
+    This file is part of MqttSql (Copyright © 2024  Guiorgy).
+    MqttSql is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+    MqttSql is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License along with Foobar. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+using MQTTnet.Diagnostics.Logger;
 using System;
 
 namespace MqttSql.Logging;
