a0718ea2b95586c5329b6dd4725e5976ba7d5bb4
Guiorgy <Guiorgy123@gmail.com>
2024-04-22 16:46:13 +0400

    parameterized .NET base docker image
    also removed custom `mqttsql` user in favor of the included `app` user, since commands can't be run in the Ubuntu Chiseled image since it's missing any shell

--- docker-build.ps1
+++ docker-build.ps1
@@ -1,30 +1,55 @@
 param(
-  [string]$COMMAND = 'build',
-  [string]$ARCH = 'auto'
+  [string]$Command = 'build',
+  [string]$Arch = 'auto',
+  [string]$Base = 'default'
 )
 
-if ($COMMAND -eq 'build') {
-  if ($arch -in 'auto', 'x86', 'x64', 'arm', 'arm64') {
-    if ($arch -eq 'auto') {
-      docker build --platform=linux --tag guiorgy/mqttsql:latest --file MqttSql\Dockerfile .
-    } else {
-      $ARCH_DOCKER_MAPPING = @{
-        "auto" = "auto"
-        "x86" = "386"
-        "x64" = "amd64"
-        "arm" = "arm/v7"
-        "arm64" = "arm64"
-      }
-      $DOCKER_ARCH = $ARCH_DOCKER_MAPPING[$ARCH]
+$TAG = 'guiorgy/mqttsql'
 
-      docker build --platform=linux/$DOCKER_ARCH --tag guiorgy/mqttsql:latest --file MqttSql\Dockerfile .
+if ($Command -eq 'build') {
+  if ($Arch -in 'auto', 'x86', 'x64', 'arm', 'arm64') {
+    $ARCH_DOCKER_PLATFORM_MAPPING = @{
+      "auto" = "linux"
+      "x86" = "linux/386"
+      "x64" = "linux/amd64"
+      "arm" = "linux/arm/v7"
+      "arm64" = "linux/arm64"
     }
+
+    $PLATFORM = $ARCH_DOCKER_PLATFORM_MAPPING[$Arch]
   } else {
-    Write-Error "Invalid architecture specified: $arch. Please use one of: auto, x86, x64, arm, arm64"
+    Write-Error "Invalid architecture specified: $Arch. Please use one of: auto, x86, x64, arm, arm64"
   }
-} elseif ($COMMAND -eq 'save') {
+
+  if ($Base -in 'default', 'debian', 'ubuntu', 'ubuntu-chiseled', 'ubuntu-chiseled-extra', 'alpine') {
+    $BASE_SDK_IMAGE_TAG_MAPPING = @{
+      "default" = "8.0"
+      "debian" = "8.0"
+      "ubuntu" = "8.0-jammy"
+      "ubuntu-chiseled" = "8.0-jammy"
+      "ubuntu-chiseled-extra" = "8.0-jammy"
+      "alpine" = "8.0-alpine"
+    }
+
+    $BASE_RUNTIME_IMAGE_TAG_MAPPING = @{
+      "default" = "8.0"
+      "debian" = "8.0"
+      "ubuntu" = "8.0-jammy"
+      "ubuntu-chiseled" = "8.0-jammy-chiseled"
+      "ubuntu-chiseled-extra" = "8.0-jammy-chiseled-extra"
+      "alpine" = "8.0-alpine"
+    }
+
+    $SDK_TAG = $BASE_SDK_IMAGE_TAG_MAPPING[$Base]
+    $RUNTIME_TAG = $BASE_RUNTIME_IMAGE_TAG_MAPPING[$Base]
+  } else {
+    Write-Error "Invalid base image specified: $Base. Please use one of: default, debian, ubuntu, ubuntu-chiseled, ubuntu-chiseled-extra, alpine"
+  }
+
+  docker build --platform=$PLATFORM --build-arg SDK_TAG=$SDK_TAG --build-arg RUNTIME_TAG=$RUNTIME_TAG --tag "$($TAG):latest" --file MqttSql\Dockerfile .
+} elseif ($Command -eq 'save') {
   New-Item -ItemType Directory -Force -Path .\Publish\Docker
-  docker save -o .\Publish\Docker\guiorgy-mqttsql-latest.tar guiorgy/mqttsql:latest
+  docker save -o .\Publish\Docker\$($TAG -replace '/', '-')-latest.tar "$($TAG):latest"
 } else {
-  Write-Error "Invalid command specified: $command. Please use one of: 'build', 'save'"
+  Write-Error "Invalid command specified: $Command. Please use one of: 'build', 'save'"
 }
