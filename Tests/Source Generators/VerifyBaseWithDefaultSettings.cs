e788a80e35d937769e6bf973843c5c423733585d
Guiorgy <Guiorgy123@gmail.com>
2024-05-27 23:53:54 +0400

    added tests for source generators

---
+++ Tests/Source Generators/VerifyBaseWithDefaultSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using VerifyMSTest;
+using VerifyTests;
+
+namespace Tests.Source_Generators;
+
+public abstract class VerifyBaseWithDefaultSettings : VerifyBase
+{
+    [ModuleInitializer]
+    public static void Initialize() => VerifyDiffPlex.Initialize();
+
+    protected static VerifySettings GetDefaultVerifySettings()
+    {
+        VerifySettings settings = new();
+        settings.UseDiffPlex();
+        settings.UseDirectory("VerifySnapshots");
+        return settings;
+    }
+
+    protected static readonly VerifySettings DefaultVerifySettings = GetDefaultVerifySettings();
+
+    public SettingsTask Verify(string? target, VerifySettings? settings = null) => base.Verify(target, settings ?? DefaultVerifySettings);
+}
