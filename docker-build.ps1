9d7e0a6619c7a05fc561c6fcd5b4654f1c42c3cb
Guiorgy <Guiorgy123@gmail.com>
2025-01-21 15:52:56 +0400

    added help output to the docker-build.ps1 script

--- docker-build.ps1
+++ docker-build.ps1
@@ -11,7 +11,7 @@ param(
   [string]$Base = 'default'
 )
 
-$TAG = 'guiorgy/mqttsql'
+$IMAGE_TAG = 'guiorgy/mqttsql'
 
 $ErrorActionPreference = 'Stop'
 $InformationPreference = 'Continue'
@@ -48,61 +48,100 @@ function Get-AllKeysFromMapping {
   return $keys | Sort-Object
 }
 
-if ($Command -eq 'build') {
-  $ARCH_DOCKER_PLATFORM_MAPPING = @{
-    'linux' = @('auto')
-    'linux/386' = @('x86')
-    'linux/amd64' = @('x64')
-    'linux/arm/v7' = @('arm')
-    'linux/arm64' = @('arm64')
-  }
+$ARCH_DOCKER_PLATFORM_MAPPING = @{
+  'linux' = @('auto')
+  'linux/386' = @('x86')
+  'linux/amd64' = @('x64')
+  'linux/arm/v7' = @('arm')
+  'linux/arm64' = @('arm64')
+}
 
+$BASE_SDK_IMAGE_TAG_MAPPING = @{
+  '9.0' = @('default', 'debian')
+  '9.0-noble' = @('ubuntu', 'ubuntu-chiseled', 'ubuntu-chiseled-extra', 'ubuntu-24', 'ubuntu-24-chiseled', 'ubuntu-24-chiseled-extra')
+  '9.0-alpine' = @('alpine')
+}
+
+$BASE_RUNTIME_IMAGE_TAG_MAPPING = @{
+  '9.0' = @('default', 'debian')
+  '9.0-noble' = @('ubuntu', 'ubuntu-24')
+  '9.0-noble-chiseled' = @('ubuntu-chiseled', 'ubuntu-24-chiseled')
+  '9.0-noble-chiseled-extra' = @('ubuntu-chiseled-extra', 'ubuntu-24-chiseled-extra')
+  '9.0-alpine' = @('alpine')
+}
+
+if ($Command -eq 'build') {
   $PLATFORM = Get-ValueFromMapping -Mapping $ARCH_DOCKER_PLATFORM_MAPPING -Key $Arch
 
   if (-not $PLATFORM) {
     Write-Error "Invalid architecture specified: $Arch. Please use one of: $($(Get-AllKeysFromMapping -Mapping $ARCH_DOCKER_PLATFORM_MAPPING) -join ', ')"
   }
 
-  $BASE_SDK_IMAGE_TAG_MAPPING = @{
-    '9.0' = @('default', 'debian')
-    '9.0-noble' = @('ubuntu', 'ubuntu-chiseled', 'ubuntu-chiseled-extra', 'ubuntu-24', 'ubuntu-24-chiseled', 'ubuntu-24-chiseled-extra')
-    '9.0-alpine' = @('alpine')
-  }
+  $SDK_IMAGE_TAG = Get-ValueFromMapping -Mapping $BASE_SDK_IMAGE_TAG_MAPPING -Key $Base
+  $RUNTIME_IMAGE_TAG = Get-ValueFromMapping -Mapping $BASE_RUNTIME_IMAGE_TAG_MAPPING -Key $Base
 
-  $BASE_RUNTIME_IMAGE_TAG_MAPPING = @{
-    '9.0' = @('default', 'debian')
-    '9.0-noble' = @('ubuntu', 'ubuntu-24')
-    '9.0-noble-chiseled' = @('ubuntu-chiseled', 'ubuntu-24-chiseled')
-    '9.0-noble-chiseled-extra' = @('ubuntu-chiseled-extra', 'ubuntu-24-chiseled-extra')
-    '9.0-alpine' = @('alpine')
-  }
-
-  $SDK_TAG = Get-ValueFromMapping -Mapping $BASE_SDK_IMAGE_TAG_MAPPING -Key $Base
-  $RUNTIME_TAG = Get-ValueFromMapping -Mapping $BASE_RUNTIME_IMAGE_TAG_MAPPING -Key $Base
-
-  if (-not $SDK_TAG -or -not $RUNTIME_TAG) {
+  if (-not $SDK_IMAGE_TAG -or -not $RUNTIME_IMAGE_TAG) {
     Write-Error "Invalid base image specified: $Base. Please use one of: $($(Get-AllKeysFromMapping -Mapping $BASE_RUNTIME_IMAGE_TAG_MAPPING) -join ', ')"
   }
 
-  Write-Information "Building image '$($TAG):latest'"
+  Write-Information "Building image '$($IMAGE_TAG):latest'"
 
-  docker build --platform=$PLATFORM --build-arg SDK_TAG=$SDK_TAG --build-arg RUNTIME_TAG=$RUNTIME_TAG --tag "$($TAG):latest" --file MqttSql\Dockerfile .
+  docker build --platform=$PLATFORM --build-arg SDK_TAG=$SDK_IMAGE_TAG --build-arg RUNTIME_TAG=$RUNTIME_IMAGE_TAG --tag "$($IMAGE_TAG):latest" --file MqttSql\Dockerfile .
 
-  Write-Information "Image '$($TAG):latest' built"
+  Write-Information "Image '$($IMAGE_TAG):latest' built"
 } elseif ($Command -eq 'save') {
   New-Item -ItemType Directory -Force -Path .\Publish\Docker > $null
-  $ARCHIVE = ".\Publish\Docker\$($TAG -replace '/', '-')-latest.tar.gz"
+  $ARCHIVE = ".\Publish\Docker\$($IMAGE_TAG -replace '/', '-')-latest.tar.gz"
 
   Write-Information "Saving image to '$ARCHIVE'"
 
   if ($PSVersionTable.PSVersion.Major -ge 7) {
-    docker save "$($TAG):latest" | gzip --best --stdout --verbose > "$ARCHIVE"
+    docker save "$($IMAGE_TAG):latest" | gzip --best --stdout --verbose > "$ARCHIVE"
   } else {
     # PowerShell 5 doesn't handle byte streams properly resulting in a corrupted archive
-    cmd /c "docker save $($TAG):latest | gzip --best --stdout --verbose > $ARCHIVE"
+    cmd /c "docker save $($IMAGE_TAG):latest | gzip --best --stdout --verbose > $ARCHIVE"
   }
 
   Write-Information "Image saved to '$ARCHIVE'"
+} elseif ($Command -eq 'help') {
+  Write-Host "Description:" -ForegroundColor Yellow
+  Write-Host "  This script builds, saves, and manages Docker images for $($IMAGE_TAG).`n" -ForegroundColor Green
+
+  $SCRIPT_NAME = $MyInvocation.MyCommand.Name
+  Write-Host "Usage:" -ForegroundColor Yellow
+  Write-Host "  .\$SCRIPT_NAME -Command <command> -Arch <architecture> -Base <base tag>" -ForegroundColor Green
+  Write-Host "  .\$SCRIPT_NAME <command>" -ForegroundColor Green
+  Write-Host "  .\$SCRIPT_NAME <command> <architecture>" -ForegroundColor Green
+  Write-Host "  .\$SCRIPT_NAME <command> <architecture> <base tag>`n" -ForegroundColor Green
+
+  Write-Host "Note:" -ForegroundColor Yellow
+  Write-Host "  When executing this script from cmd.exe, arguments are not passed correctly, so the default arguments will be used (highlighted with square brackets below []).`n" -ForegroundColor Red
+
+  Write-Host "Commands:" -ForegroundColor Yellow
+  Write-Host "  [build]  - Build the Docker image." -ForegroundColor Green
+  Write-Host "  save     - Save the Docker image to an archive." -ForegroundColor Green
+  Write-Host "  help     - Display this help page.`n" -ForegroundColor Green
+
+  Write-Host "Options:" -ForegroundColor Yellow
+  Write-Host "  -Arch: Architecture of the Docker image to build." -ForegroundColor Green
+  $ARCH_OPTION_VALUES = (Get-AllKeysFromMapping -Mapping $ARCH_DOCKER_PLATFORM_MAPPING) | ForEach-Object { if ($_ -eq 'auto') { "[$_]" } else { "$_" } }
+  Write-Host "    $($ARCH_OPTION_VALUES -join ', ')`n" -ForegroundColor Green
+
+  Write-Host "  -Base: Base image tag for the Docker image." -ForegroundColor Green
+  $DEFAULT_IMAGE_TAG = Get-ValueFromMapping -Mapping $BASE_SDK_IMAGE_TAG_MAPPING -Key "default"
+  $BASE_OPTION_VALUES = @()
+  foreach ($TAG in $BASE_RUNTIME_IMAGE_TAG_MAPPING.Keys) {
+    foreach ($BASE in $BASE_RUNTIME_IMAGE_TAG_MAPPING[$TAG]) {
+      if ($TAG -eq "$DEFAULT_IMAGE_TAG") {
+        if ($BASE -ne 'default') {
+          $BASE_OPTION_VALUES += "[$BASE]"
+        }
+      } else {
+        $BASE_OPTION_VALUES += $BASE
+      }
+    }
+  }
+  Write-Host "    $($BASE_OPTION_VALUES -join ', ')`n" -ForegroundColor Green
 } else {
-  Write-Error "Invalid command specified: $Command. Please use one of: 'build', 'save'"
+  Write-Error "Invalid command specified: $Command. Please use one of: 'build', 'save', 'help'"
 }
